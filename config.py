20c02461a5701cf9e464c20e4cc78a87d4d55ea3
Abbos Nazarov <104083967+abbos7788@users.noreply.github.com>
2022-08-06 17:47:52 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '5363152133:AAHNDsuevJFGE92vWFrHCG4UYSScHO4PTa0'
+# chat_id = '-1001722090137'
+
+
+# "O'zbekiston temir yo'llari" aksiyadorlik jamiyati miqyosidagi barcha tarkibiy bo'linmalarning xizmat telefon raqamlarining qidiruv tizimi.
+
+# "Qarshi mintaqaviy temir yo'l uzeli" UK
